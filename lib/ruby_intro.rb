20c6c83a689d0cd1e75bd2f8e441e793aa2522da
EC2 Default User <ec2-user@ip-172-31-22-242.ec2.internal>
2021-02-07 02:49:25 +0000

    hw1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,94 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  elsif arr[1] > arr[0]
+    max_val = arr[1]
+    second_max_val = arr[0]
+  else
+    max_val = arr[0]
+    second_max_val = arr[1]
+  end
+  
+  for i in 2..arr.length-1 do
+    if arr[i] > max_val
+      second_max_val = max_val
+      max_val = arr[i]
+    elsif arr[i] > second_max_val
+      second_max_val = arr[i]
+    end
+  end
+  max_val + second_max_val
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length <= 1
+    return false
+  end
+  
+  for i in 0..arr.length-1 do
+    for j in 0..arr.length-1 do
+      if arr[i] + arr[j] == n && i != j
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s == ""
+    return false
+  elsif s[0] =~ /[\WaeiouAEIOU]/
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return s =~ /^[10]*00$/ || s == '0'
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new(
+      "Expected valid ISBN number"
+    ) if isbn == ""
+    
+    raise ArgumentError.new(
+      "Expected valid price"
+    ) if price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    if (price.to_s =~ /\.\d\d/)
+      return "$" + price.to_s
+    elsif (price.to_s =~ /\.\d$/)
+      return "$" + price.to_s + "0"
+    else
+      return "$" + price.to_s + ".00"
+    end
+  end
 end
